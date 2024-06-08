f16069b9df9a9c168efe4b1913f6d4e6238cd7ef
siraom15 <siriwatjaiyungyuen@gmail.com>
2024-06-08 22:41:53 +0700

    Add components

--- components/story/story-list.tsx
+++ components/story/story-list.tsx
@@ -5,16 +5,21 @@ import { Input } from "../ui/input";
 import { Button } from "../ui/button";
 import { useState } from "react";
 import { Loading } from "../ui/loading";
-import { StoryCreationDialog } from "./story-creation";
+import { StoryCreationDialog } from "./story-creation-dialog";
 import Link from "next/link";
 import { StoryCard } from "./story-card";
-import { StoryEmptyPlaceholder } from "./story-empty-placeholder";
+import { EmptyPlaceholder } from "../ui/empty-placeholder";
 
 interface StoryListProps {
   stories: StoryWithProfile[];
   isLoading: boolean;
+  showCreateStory?: boolean;
 }
-export const StoryList = ({ stories, isLoading }: StoryListProps) => {
+export const StoryList = ({
+  stories,
+  isLoading,
+  showCreateStory = true,
+}: StoryListProps) => {
   const [search, setSearch] = useState<string>("");
   // Filter stories by search
   const filteredStories = stories.filter((story) =>
@@ -29,19 +34,27 @@ export const StoryList = ({ stories, isLoading }: StoryListProps) => {
         <div className="flex w-full max-w-sm items-center space-x-2 my-2">
           <Input
             type="text"
-            // className="focus:ring-violet-400 focus:border-violet-400 w-full border-violet-400 active:border-violet-400"
             placeholder="Story Name"
             value={search}
             onChange={(e) => setSearch(e.target.value)}
           />
-          {search && <Button className=" bg-gradient-to-r from-violet-400 to-pink-400 hover:bg-gradient-to-tr" onClick={() => setSearch("")}>Clear</Button>}
+          {search && (
+            <Button
+              className=" bg-gradient-to-r from-violet-400 to-pink-400 hover:bg-gradient-to-tr"
+              onClick={() => setSearch("")}
+            >
+              Clear
+            </Button>
+          )}
         </div>
       </div>
       {isLoading ? (
         <Loading type="dot" />
       ) : (
         <div className="grid grid-cols-6 gap-2">
-          {!search && <StoryCreationDialog className="w-full min-h[150px]" />}
+          {!search && showCreateStory && (
+            <StoryCreationDialog className="w-full min-h[150px]" />
+          )}
           {filteredStories.length > 0 ? (
             filteredStories.map((story: StoryWithProfile) => (
               <Link href={`/app/story/${story.id}`} key={story.id}>
@@ -56,7 +69,11 @@ export const StoryList = ({ stories, isLoading }: StoryListProps) => {
               </Link>
             ))
           ) : (
-            <StoryEmptyPlaceholder className="col-span-6" />
+            <EmptyPlaceholder
+              className="col-span-6"
+              title="No Story Founded"
+              description="Start by creating a new story."
+            />
           )}
         </div>
       )}
