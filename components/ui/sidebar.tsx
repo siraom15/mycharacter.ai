184ccdad08affcb95ee6f98085386652f7ae4476
siraom15 <siriwatjaiyungyuen@gmail.com>
2024-06-08 17:51:39 +0700

    Refactor sidebar

--- components/ui/sidebar.tsx
+++ components/ui/sidebar.tsx
@@ -10,106 +10,126 @@ import {
   HashtagIcon,
   UserCircleIcon,
 } from "@heroicons/react/24/outline";
-import {
-  BookmarkFilledIcon,
-  BookmarkIcon,
-  ListBulletIcon,
-} from "@radix-ui/react-icons";
+import { BookmarkIcon, ListBulletIcon } from "@radix-ui/react-icons";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useState } from "react";
 
 interface SidebarProps extends React.HTMLAttributes<HTMLElement> {}
 
+interface NavItemProps {
+  href?: string;
+  icon: React.ElementType;
+  text: string;
+  variant?:
+    | "default"
+    | "destructive"
+    | "outline"
+    | "secondary"
+    | "ghost"
+    | "link";
+  className?: string;
+  type?: "button" | "submit" | "reset";
+}
+
+const NavItem = ({
+  href,
+  icon: Icon,
+  text,
+  variant = "ghost",
+  className = "",
+  type = "button",
+  ...props
+}: NavItemProps) => {
+  const content = (
+    <Button
+      variant={variant}
+      className={`w-full justify-start ${className}`}
+      type={type}
+      {...props}
+    >
+      <Icon className="mr-2 h-4 w-4" />
+      {text}
+    </Button>
+  );
+
+  return href ? <Link href={href}>{content}</Link> : content;
+};
+
+const Section = ({
+  title,
+  children,
+}: {
+  title: string;
+  children: React.ReactNode;
+}) => (
+  <div className="px-3 py-2">
+    <h2 className="mb-2 px-4 text-lg font-semibold tracking-tight">{title}</h2>
+    <div className="space-y-1">{children}</div>
+  </div>
+);
+
 export function Sidebar({ className, ...props }: SidebarProps) {
   const pathname = usePathname();
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const variant = (pn: string) => {
+    return pathname === pn ? "secondary" : "ghost";
+  };
 
   return (
-    <div className={cn("pb-12", className)}>
+    <div className={cn("pb-12", className)} {...props}>
       <div className="space-y-4 py-4">
-        <div className="px-3 py-2">
-          <h2 className="mb-2 px-4 text-lg font-semibold tracking-tight">
-            Discover
-          </h2>
-          <div className="space-y-1">
-            <Link href="/app/all-story">
-              <Button variant="secondary" className="w-full justify-start">
-                <BookOpenIcon className="mr-2 h-4 w-4" />
-                All Stories
-              </Button>
-            </Link>
-            <Button variant="ghost" className="w-full justify-start">
-              <FireIcon className="mr-2 h-4 w-4 text-yellow-500" />
-              Trending Story
-            </Button>
-            <Button variant="ghost" className="w-full justify-start">
-              <FireIcon className="mr-2 h-4 w-4 text-red-500" />
-              Trending Characters
-            </Button>
-          </div>
-        </div>
-        <div className="px-3 py-2">
-          <h2 className="mb-2 px-4 text-lg font-semibold tracking-tight">
-            Library
-          </h2>
-          <div className="space-y-1">
-            <Link href="/app/my-story">
-              <Button variant="ghost" className="w-full justify-start">
-                <ListBulletIcon className="mr-2 h-4 w-4" />
-                My Stories
-              </Button>
-            </Link>
-            <Button variant="ghost" className="w-full justify-start">
-              <BookmarkIcon className="mr-2 h-4 w-4" />
-              Saved Stories
-            </Button>
-            <Link href="/app/generate-character">
-              <Button variant="ghost" className="w-full justify-start">
-                <BoltIcon className="mr-2 h-4 w-4" />
-                Quick Generate Character
-              </Button>
-            </Link>
-          </div>
-        </div>
+        <Section title="Discover">
+          <NavItem
+            href="/app/all-story"
+            icon={BookOpenIcon}
+            text="All Stories"
+            variant={variant("/app/all-story")}
+          />
+          <NavItem
+            icon={FireIcon}
+            text="Trending Story"
+            variant={variant("/app/trending-story")}
+          />
+          <NavItem
+            icon={FireIcon}
+            text="Trending Characters"
+            variant={variant("/app/trending-characters")}
+          />
+        </Section>
 
-        <div className="px-3 py-2">
-          <h2 className="mb-2 px-4 text-lg font-semibold tracking-tight">
-            Pinned Stories
-          </h2>
-          <div className="space-y-1">
-            <Button variant="ghost" className="w-full justify-start">
-              <HashtagIcon className="mr-2 h-4 w-4" />
-              Lover
-            </Button>
-            <Button variant="ghost" className="w-full justify-start">
-              <HashtagIcon className="mr-2 h-4 w-4" />
-              Sakura Love
-            </Button>
-          </div>
-        </div>
+        <Section title="Library">
+          <NavItem
+            href="/app/my-story"
+            icon={ListBulletIcon}
+            text="My Stories"
+            variant={variant("/app/my-story")}
+          />
+          <NavItem icon={BookmarkIcon} text="Saved Stories" />
+          <NavItem
+            href="/app/generate-character"
+            icon={BoltIcon}
+            text="Quick Generate Character"
+            variant={variant("/app/generate-character")}
+          />
+        </Section>
 
-        <div className="px-3 py-2">
-          <h2 className="mb-2 px-4 text-lg font-semibold tracking-tight">
-            Settings
-          </h2>
-          <div className="space-y-1">
-            <Link href="/app/profile">
-              <Button variant="ghost" className="w-full justify-start">
-                <UserCircleIcon className="mr-2 h-4 w-4" />
-                Profile
-              </Button>
-            </Link>
-            <form action="/auth/signout" method="post">
-              <Button
-                type="submit"
-                variant="ghost"
-                className="w-full justify-start"
-              >
-                <ArrowLeftStartOnRectangleIcon className="mr-2 h-4 w-4" />
-                Signout
-              </Button>
-            </form>
-          </div>
-        </div>
+        <Section title="Pinned Stories">
+          <NavItem icon={HashtagIcon} text="Lover" />
+          <NavItem icon={HashtagIcon} text="Sakura Love" />
+        </Section>
+
+        <Section title="Settings">
+          <NavItem href="/app/profile" icon={UserCircleIcon} text="Profile" />
+          <form action="/auth/signout" method="post">
+            <NavItem
+              icon={ArrowLeftStartOnRectangleIcon}
+              text="Signout"
+              type="submit"
+            />
+          </form>
+        </Section>
       </div>
     </div>
   );
