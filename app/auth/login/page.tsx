8c049b0470fb9092396601ca6ecd0f4292b96244
siraom15 <siriwatjaiyungyuen@gmail.com>
2024-06-08 23:21:38 +0700

    style: 💄 Update Login/Signin UO

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -14,7 +14,7 @@ export const metadata: Metadata = {
 export default function AuthenticationPage() {
   return (
     <>
-      <div className="container relative h-[800px] flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+      <div className="container relative h-[100vh] flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
         <div className="relative hidden h-full flex-col bg-muted p-10 text-white lg:flex dark:border-r">
           <div className="absolute inset-0 bg-gradient-to-r from-teal-400 to-yellow-200" />
           <div className="relative z-20 flex items-center text-lg font-medium">
