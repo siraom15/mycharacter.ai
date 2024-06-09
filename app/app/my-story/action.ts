58677b9687fbf287458edae8a0b065a7f7c73cb8
siraom15 <siriwatjaiyungyuen@gmail.com>
2024-06-09 19:59:08 +0700

    Add Update Story

--- app/app/my-story/action.ts
+++ app/app/my-story/action.ts
@@ -8,7 +8,13 @@ interface StoryCreationProps {
     description: string;
     is_public: boolean;
 }
+
+interface StoryUpdateProps extends StoryCreationProps {
+    id: string;
+}
+
 interface StoryCreationResult {
+    data?: Story;
     error: boolean;
     errorMessage?: string;
 }
@@ -26,7 +32,32 @@ export async function createStory({ name, description, is_public }: StoryCreatio
             is_public,
         };
 
-        const { error } = await supabase.from('stories').insert(data);
+        const { error, data: created_story } = await supabase.from('stories').upsert(data).select().single();
+        if (error) {
+            throw error;
+        }
+
+        return { error: false, data: created_story };
+    } catch (error: any) {
+        console.error(error.message);
+        return { errorMessage: error.message, error: true };
+    }
+}
+
+export async function updateStory({ id, name, description, is_public }: StoryUpdateProps): Promise<StoryCreationResult> {
+    const supabase = createClient();
+
+    try {
+        const { data: { user } } = await supabase.auth.getUser();
+        const ownerId = user?.id;
+        const data = {
+            name,
+            description,
+            owner: ownerId,
+            is_public,
+        };
+
+        const { error } = await supabase.from('stories').update(data).eq('id', id);
         if (error) {
             throw error;
         }
