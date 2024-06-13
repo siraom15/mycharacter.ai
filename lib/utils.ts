5b95fcb50673b179e45401b7fd7844aad5eb8772
siraom15 <siriwatjaiyungyuen@gmail.com>
2024-06-13 23:50:07 +0700

    Add lib function

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,8 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function parseBase64Image(base64: string): string {
+  if (!base64) return "";
+  return `data:image/png;base64,${base64}`;
+};
