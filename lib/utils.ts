77afcd0575d5cbf5ab6a74ae85cf7dcf2d73b20e
siraom15 <siriwatjaiyungyuen@gmail.com>
2024-06-14 10:06:04 +0700

    Update Utils

--- lib/utils.ts
+++ lib/utils.ts
@@ -9,3 +9,13 @@ export function parseBase64Image(base64: string): string {
   if (!base64) return "";
   return `data:image/png;base64,${base64}`;
 };
+
+export function base64ToImageFile(base64: string, filename: string): File {
+  const byteString = atob(base64);
+  const ab = new ArrayBuffer(byteString.length);
+  const ia = new Uint8Array(ab);
+  for (let i = 0; i < byteString.length; i++) {
+    ia[i] = byteString.charCodeAt(i);
+  }
+  return new File([ab], filename, { type: "image/png" });
+}
